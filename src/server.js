071d3307a6552dcf31f177b53bd3119bd9a1d37b
Felipe Coutinho <felipe04coutinho@gmail.com>
2021-04-01 02:11:41 -0300

    aula 02
    configuraçao das rotas de criar, salvar, editar, atualizar os jobs, rota de atualizar o profile
    
    atualizaçao dos dados dinamicos nos arquivos ejs

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ import routes from './routes.js';
 
 const server = express();
 
+// habilitar template engine
 server.set('view engine', 'ejs');
 
+// habilitar arquivos estaticos
 server.use(express.static('public'));
+
+// habilitar req.body
+server.use(express.urlencoded({ extended: true }));
+
+// utilizar as rotas
 server.use(routes);
 
 server.listen(3000, () => console.log('rodando'));
